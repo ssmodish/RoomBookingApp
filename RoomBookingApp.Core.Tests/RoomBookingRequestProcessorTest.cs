14b551d41b5b8edbaf3422d2bbec2dd5bc4e9ef6
Setve Smodish <stevesmodish@gmail.com>
2025-05-15 14:24:52 -0400

    Completed Section 3

--- RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -1,4 +1,9 @@
-﻿using RoomBookingApp.Core.Models;
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using Moq;
+using RoomBookingApp.Core.DataServices;
+using RoomBookingApp.Core.Domain;
+using RoomBookingApp.Core.Enums;
+using RoomBookingApp.Core.Models;
 using RoomBookingApp.Core.Processors;
 using Shouldly;
 using System;
@@ -10,24 +15,42 @@ using Xunit;
 
 namespace RoomBookingApp.Core
 {
+
     public class RoomBookingRequestProcessorTest
     {
-        [Fact]
-        public void Should_Return_Room_Booking_Response_With_Request_Values()
+        private RoomBookingRequestProcessor _processor;
+        private RoomBookingRequest _request;
+        private Mock<IRoomBookingService> _roomBookingServiceMock;
+        private List<Room> _availableRooms;
+
+        public RoomBookingRequestProcessorTest()
         {
-            // Arrange - AKA Mock data 
-            var bookingRequest = new RoomBookingRequest
+            // Arrange for this block of tests
+            _request = new RoomBookingRequest
             {
                 FullName = "Test Name",
                 Email = "test@request.com",
                 Date = new DateTime(2021, 10, 20)
             };
 
-            var processor = new RoomBookingRequestProcessor();
+            _availableRooms = new List<Room>() { new Room() { Id = 1 } };
+
+            _roomBookingServiceMock = new Mock<IRoomBookingService>();
+
+            _roomBookingServiceMock.Setup(q => q.GetAvailableRooms(_request.Date)).Returns(_availableRooms);
+
+            _processor = new RoomBookingRequestProcessor(_roomBookingServiceMock.Object);
+        }
+
+
+        [Fact]
+        public void Should_Return_Room_Booking_Response_With_Request_Values()
+        {
+            // Arrange for this specific test - AKA Mock data - if the same data will be needed for many tests, add it to the constructor
 
 
             // Act
-            RoomBookingResult result = processor.BookRoom(bookingRequest);
+            RoomBookingResult result = _processor.BookRoom(_request);
 
             // Assert - Xunit built in package
             //Assert.NotNull(result);
@@ -37,12 +60,84 @@ namespace RoomBookingApp.Core
 
             // Assert using Shouldly package
             result.ShouldNotBeNull();
-            result.FullName.ShouldBe(bookingRequest.FullName);
-            result.Email.ShouldBe(bookingRequest.Email);
-            result.Date.ShouldBe(bookingRequest.Date);
+            result.FullName.ShouldBe(_request.FullName);
+            result.Email.ShouldBe(_request.Email);
+            result.Date.ShouldBe(_request.Date);
+        }
 
+        [Fact]
+        public void Should_Throw_Exception_For_Null_Request()
+        {
+            var exception = Should.Throw<ArgumentNullException>(() => _processor.BookRoom(null));
 
+            exception.ParamName.ShouldBe("bookingRequest");
+        }
 
+        [Fact]
+        public void Should_Save_Room_Booking_Request()
+        {
+            RoomBooking savedBooking = null;
+
+            _roomBookingServiceMock.Setup(q => q.Save(It.IsAny<RoomBooking>()))
+                .Callback<RoomBooking>(booking => savedBooking = booking);
+
+            _processor.BookRoom(_request);
+
+            _roomBookingServiceMock.Verify(q => q.Save(It.IsAny<RoomBooking>()), Times.Once);
+
+            savedBooking.ShouldNotBeNull();
+            savedBooking.FullName.ShouldBe(_request.FullName);
+            savedBooking.Email.ShouldBe(_request.Email);
+            savedBooking.Date.ShouldBe(_request.Date);
+            savedBooking.RoomId.ShouldBe(_availableRooms.First().Id);
+        }
+
+        [Fact]
+        public void Should_Not_Save_Room_Booking_Request_If_None_Available()
+        {
+            _availableRooms.Clear();
+            _processor.BookRoom(_request);
+            _roomBookingServiceMock.Verify(q => q.Save(It.IsAny<RoomBooking>()), Times.Never);
+
+        }
+
+        [Theory]
+        [InlineData(BookingResultFlag.Failure, false)]
+        [InlineData(BookingResultFlag.Success, true)]
+        public void Should_Return_SuccessFailure_Flag_In_Result(BookingResultFlag bookingSuccessFlag, bool isAvailable)
+        {
+            if (!isAvailable)
+            {
+                _availableRooms.Clear();
+
+            }
+
+            var result = _processor.BookRoom(_request);
+            bookingSuccessFlag.ShouldBe(result.Flag);
+
+        }
+
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(null, false)]
+        public void Should_Return_RoomBookingId_In_Result(int? roomBookingId, bool isAvailable)
+        {
+            if (!isAvailable)
+            {
+                _availableRooms.Clear();
+
+            }
+            else
+            {
+                _roomBookingServiceMock.Setup(q => q.Save(It.IsAny<RoomBooking>()))
+                    .Callback<RoomBooking>(booking => 
+                    {
+                        booking.Id = roomBookingId.Value;
+                    });
+            }
+
+            var result = _processor.BookRoom(_request);
+            result.RoomBookingId.ShouldBe(roomBookingId);
         }
     }
 }
---
+++ RoomBookingApp.Core/Domain/RoomBooking.cs
@@ -0,0 +1,10 @@
+﻿using RoomBookingApp.Core.Models;
+
+namespace RoomBookingApp.Core.Domain
+{
+    public class RoomBooking : RoomBookingBase
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+    }
+}
